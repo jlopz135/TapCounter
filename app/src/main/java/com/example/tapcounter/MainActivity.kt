1366b51295d25e00b044b6d16c96fbb1561917ec
jlopz135 <juanclopez135@gmail.com>
2023-01-26 15:26:23 -0500

    Button Adds when clicked

--- app/src/main/java/com/example/tapcounter/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.tapcounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.buttonClicker)
+
+        button.setOnClickListener {
+            Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+        }
+
+        val textView = findViewById<TextView>(R.id.textView3)
+        var counter = 0
+
+        button.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+        }
     }
 
 }
