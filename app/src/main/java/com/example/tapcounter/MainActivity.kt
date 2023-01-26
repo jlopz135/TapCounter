cb1f5f4de1630ac353a037afc01aab348d2eb9b1
jlopz135 <juanclopez135@gmail.com>
2023-01-26 15:18:55 -0500

    Initial commit, Layout Created

---
+++ app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tapcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.buttonClicker)
+    }
+
+}
